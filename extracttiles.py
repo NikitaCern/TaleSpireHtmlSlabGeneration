2a660a06674ee5e772fe3d45e4ac0289beeb720d
NikitaCern <45770000+NikitaCern@users.noreply.github.com>
2021-02-13 18:15:21 +0200

    Fixed incorrect height/width/depth info extraction

--- extracttiles.py
+++ extracttiles.py
@@ -18,23 +18,7 @@ for root,d_names,f_names in os.walk(path):
             currentFile.readline()
             asset = json.loads(currentFile.read())
             nguid = asset["GUID"]
-            asset_name = asset["boardAssetName"]
-            board_asset_group = asset["boardAssetGroup"]
-            width = 0
-            depth = 0
-            for tag in asset["Tags"]:
-                if "X" in tag:
-                    try:
-                        width = int(tag[:tag.index("X")])
-                        depth = int(tag[tag.index("X"):])
-                    except ValueError:
-                        print("Invalid tag with X")
-                        pass
-            height = asset["colliderBounds"][0]["m_Extent"]["y"]
-            if width == 0:
-                width = asset["assetLoaders"][0]["occluderInfo"]["Width"]
-            if depth == 0:
-                depth = asset["assetLoaders"][0]["occluderInfo"]["Depth"]
-            output[nguid] = {"name": asset_name, "group": board_asset_group, "width": width, "height": height, "depth": depth}
+            output[nguid] = asset
 with open("scripts/assetdata.js", "w") as fout:
     fout.write("asset_data = " + json.dumps(output) + ";")
+print("ALL Done!")
